5ab677e6e712b03bb675d8d6c02921c908e7fea4
gorob4ik <denka.gorobtsoff@gmail.com>
2019-04-14 20:20:24 +0300

    test

---
+++ header.php
@@ -0,0 +1,9 @@
+<head>
+    <script src="js/chosen/docsupport/jquery-3.2.1.min.js" type="text/javascript"></script>
+    <script src="js/chosen/chosen.jquery.js"></script>
+    <script src="js/chosen/docsupport/prism.js"></script>
+    <script src="js/chosen/docsupport/init.js"></script>
+    <link href="js/chosen/docsupport/prism.css">
+    <link href="js/chosen/docsupport/style.css">
+    <link href="js/chosen/chosen.css">
+</head>
